abedfa078885db607dd2cf1d09208baef92cb21b
MichalRac <rac.michalmail@gmail.com>
2022-11-13 21:11:24 +0100

    [TaskManager] Solve TaskManager exercise by adding tasks completed screen

--- TaskManager/app/src/main/java/com/michal_rac/taskmanager/MainActivity.kt
+++ TaskManager/app/src/main/java/com/michal_rac/taskmanager/MainActivity.kt
@@ -3,13 +3,21 @@ package com.michal_rac.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.michal_rac.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +30,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TasksCompleted()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        TasksCompleted()
+    }
+}
+
+@Composable
+fun TasksCompleted()
+{
+    Column(
+        Modifier
+            .fillMaxSize()
+            .wrapContentHeight(Alignment.CenterVertically)
+    ) {
+        Image(
+            painterResource(R.drawable.ic_task_completed),
+            contentDescription = null,
+            Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
+
+        Text(
+            stringResource(R.string.all_tasks_completed_text),
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp, bottom = 8.dp),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+        )
+
+        Text(
+            stringResource(R.string.congratulations_text),
+            Modifier.fillMaxWidth(),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
